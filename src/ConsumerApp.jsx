3beff98023f5b084aa06dab0db111683db92fd57
Duhuhuu <ignisxmizu@gmail.com>
2022-11-09 15:01:06 -0700

    Actu

--- src/ConsumerApp.jsx
+++ src/ConsumerApp.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import { PokemonApp } from "./pokemon/PokemonApp"
+import { PokemonHome } from "./pokemon"
+
 
 export const ConsumerApp = () => {
   return (
     <Routes>
  
     {/* //  ( status === 'authenticated')  */}
-        <Route path="/*" element={ <PokemonApp/>  } />
+        <Route path="/*" element={ <PokemonHome/>  } />
         {/* <Route path="/auth/*" element={ <AuthRoutes/>  } /> */}
     
      <Route path="/*" element= { <Navigate to='/' /> } />
---
+++ src/pokemon/PokemonHome.jsx
@@ -0,0 +1,18 @@
+
+export const PokemonHome = () => {
+    // style={{backgroundImage:`url("./assets/pokeImgs/pokemon-5.svg")`}
+  return (
+    
+    <>
+    <div className="poke container-xl">
+
+    <div className="d-grid gap-2">
+  <button className="btn " type="button"  >!!</button>
+</div>
+    
+
+    </div>
+    </>
+
+  )
+}
