29340d6f3dcbc252b039a86b7f216ed9282d13cd
Duhuhuu <ignisxmizu@gmail.com>
2022-11-10 15:46:48 -0700

    Home

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,12 +4,10 @@ export const AppRouter = () => {
   return (
     
    <Routes>
- 
 //  ( status === 'authenticated') 
     <Route path="/*" element={ <JournalRoutes/>  } />
     <Route path="/auth/*" element={ <AuthRoutes/>  } />
-
- <Route path="/*" element= { <Navigate to='/auth/login' /> } />
+    <Route path="/*" element= { <Navigate to='/auth/login' /> } />
 
  
 </Routes>
